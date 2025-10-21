241b5b5ad2780013af1c328e7a07f4bb2c16f1be
bdisptn <budisptn13@gmail.com>
2025-10-21 08:57:17 +0700

    No.1 Identifier

---
+++ src/Identifier/StudentInfo.java
@@ -0,0 +1,10 @@
+package Identifier;
+
+public class StudentInfo {
+    public String showStudentInfo(String name, int age, double gpa) {
+        return "Name: " + name + "\n" +
+                "Age : " + age + "\n" +
+                "GPA : " + gpa + "\n";
+
+    }
+}
