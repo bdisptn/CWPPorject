d2cd639c11d558485eaaa3717ec185b75017dff7
bdisptn <budisptn13@gmail.com>
2025-10-23 06:19:39 +0700

    No.9 Function

---
+++ src/Function1/Function1.java
@@ -0,0 +1,13 @@
+package Function1;
+
+public class Function1 {
+    // Metode untuk menjumlahkan dua bilangan
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Metode untuk mengalikan dua bilangan
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
